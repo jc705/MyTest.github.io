6f879721d5b597078251aefc465aa354c627e64f
Clark <Clark@test.com>
2024-10-12 14:48:55 +0800

    txySdk -v 0.0.3

--- src/settings.4b5be.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/test.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"8c73e",main:"57811"}};
