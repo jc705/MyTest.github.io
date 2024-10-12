32c07d2a9d5c02b60c0ec42efd9718967ca6be2f
Clark <Clark@test.com>
2024-10-12 17:02:50 +0800

    set btn

---
+++ src/settings.ecc70.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/test.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"8c73e",main:"bc9d6"}};
